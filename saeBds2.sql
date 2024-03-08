9a3169dc11548279c4cca796af936636f9b6833e
foolhardy45 <82352377+foolhardy45@users.noreply.github.com>
2024-03-08 14:47:21 +0100

    Update saeBds2.sql

--- saeBds2.sql
+++ saeBds2.sql
@@ -8,12 +8,12 @@
 -- Table: utilisateur
 ------------------------------------------------------------
 CREATE TABLE public.utilisateur(
-	id_utilisateur   INT  NOT NULL ,
-	login            VARCHAR (-1) NOT NULL ,
-	email            VARCHAR (-1) NOT NULL ,
-	nom              VARCHAR (-1) NOT NULL ,
-	pwd              VARCHAR (-1) NOT NULL ,
-	user_role        VARCHAR (-1) NOT NULL  ,
+	id_utilisateur   INT   ,
+	login            VARCHAR   ,
+	email            VARCHAR   ,
+	nom              VARCHAR   ,
+	pwd              VARCHAR   ,
+	user_role        VARCHAR    ,
 	CONSTRAINT utilisateur_PK PRIMARY KEY (id_utilisateur)
 )WITHOUT OIDS;
 
@@ -22,8 +22,8 @@ CREATE TABLE public.utilisateur(
 -- Table: etat
 ------------------------------------------------------------
 CREATE TABLE public.etat(
-	id_etat   INT  NOT NULL ,
-	libelle   VARCHAR (50) NOT NULL  ,
+	id_etat   INT   ,
+	libelle   VARCHAR (50)   ,
 	CONSTRAINT etat_PK PRIMARY KEY (id_etat)
 )WITHOUT OIDS;
 
@@ -32,9 +32,9 @@ CREATE TABLE public.etat(
 -- Table: commande
 ------------------------------------------------------------
 CREATE TABLE public.commande(
-	id_commande   INT  NOT NULL ,
-	date_achat    DATE  NOT NULL ,
-	id_etat       INT  NOT NULL  ,
+	id_commande   INT   ,
+	date_achat    DATE   ,
+	id_etat       INT    ,
 	CONSTRAINT commande_PK PRIMARY KEY (id_commande)
 
 	,CONSTRAINT commande_etat_FK FOREIGN KEY (id_etat) REFERENCES public.etat(id_etat)
@@ -45,8 +45,8 @@ CREATE TABLE public.commande(
 -- Table: materiau
 ------------------------------------------------------------
 CREATE TABLE public.materiau(
-	id_materiau        INT  NOT NULL ,
-	libelle_materiau   VARCHAR (50) NOT NULL  ,
+	id_materiau        INT   ,
+	libelle_materiau   VARCHAR (50)   ,
 	CONSTRAINT materiau_PK PRIMARY KEY (id_materiau)
 )WITHOUT OIDS;
 
@@ -55,8 +55,8 @@ CREATE TABLE public.materiau(
 -- Table: type_meuble
 ------------------------------------------------------------
 CREATE TABLE public.type_meuble(
-	id_type        INT  NOT NULL ,
-	libelle_type   VARCHAR (50) NOT NULL  ,
+	id_type        INT   ,
+	libelle_type   VARCHAR   ,
 	CONSTRAINT type_meuble_PK PRIMARY KEY (id_type)
 )WITHOUT OIDS;
 
@@ -65,14 +65,14 @@ CREATE TABLE public.type_meuble(
 -- Table: meuble
 ------------------------------------------------------------
 CREATE TABLE public.meuble(
-	id_meuble     INT  NOT NULL ,
-	nom_meuble    VARCHAR (-1) NOT NULL ,
-	largeur       INT  NOT NULL ,
-	hauteur       INT  NOT NULL ,
-	prix_meuble   FLOAT  NOT NULL ,
-	fournisseur   VARCHAR (-1) NOT NULL ,
-	marque        VARCHAR (-1) NOT NULL ,
-	id_type       INT  NOT NULL  ,
+	id_meuble     INT   ,
+	nom_meuble    VARCHAR   ,
+	largeur       INT   ,
+	hauteur       INT   ,
+	prix_meuble   FLOAT   ,
+	fournisseur   VARCHAR   ,
+	marque        VARCHAR   ,
+	id_type       INT    ,
 	CONSTRAINT meuble_PK PRIMARY KEY (id_meuble)
 
 	,CONSTRAINT meuble_type_meuble_FK FOREIGN KEY (id_type) REFERENCES public.type_meuble(id_type)
@@ -83,8 +83,8 @@ CREATE TABLE public.meuble(
 -- Table: contenir
 ------------------------------------------------------------
 CREATE TABLE public.contenir(
-	id_commande   INT  NOT NULL ,
-	id_meuble     INT  NOT NULL  ,
+	id_commande   INT   ,
+	id_meuble     INT    ,
 	CONSTRAINT contenir_PK PRIMARY KEY (id_commande,id_meuble)
 
 	,CONSTRAINT contenir_commande_FK FOREIGN KEY (id_commande) REFERENCES public.commande(id_commande)
@@ -96,8 +96,8 @@ CREATE TABLE public.contenir(
 -- Table: etre_fait
 ------------------------------------------------------------
 CREATE TABLE public.etre_fait(
-	id_materiau   INT  NOT NULL ,
-	id_meuble     INT  NOT NULL  ,
+	id_materiau   INT   ,
+	id_meuble     INT    ,
 	CONSTRAINT etre_fait_PK PRIMARY KEY (id_materiau,id_meuble)
 
 	,CONSTRAINT etre_fait_materiau_FK FOREIGN KEY (id_materiau) REFERENCES public.materiau(id_materiau)
@@ -109,10 +109,10 @@ CREATE TABLE public.etre_fait(
 -- Table: ligne_commande
 ------------------------------------------------------------
 CREATE TABLE public.ligne_commande(
-	id_meuble     INT  NOT NULL ,
-	id_commande   INT  NOT NULL ,
-	prix          INT  NOT NULL ,
-	quantite      INT  NOT NULL  ,
+	id_meuble     INT   ,
+	id_commande   INT   ,
+	prix          INT   ,
+	quantite      INT    ,
 	CONSTRAINT ligne_commande_PK PRIMARY KEY (id_meuble,id_commande)
 
 	,CONSTRAINT ligne_commande_meuble_FK FOREIGN KEY (id_meuble) REFERENCES public.meuble(id_meuble)
@@ -124,8 +124,8 @@ CREATE TABLE public.ligne_commande(
 -- Table: a_pour_commande
 ------------------------------------------------------------
 CREATE TABLE public.a_pour_commande(
-	id_commande      INT  NOT NULL ,
-	id_utilisateur   INT  NOT NULL  ,
+	id_commande      INT   ,
+	id_utilisateur   INT    ,
 	CONSTRAINT a_pour_commande_PK PRIMARY KEY (id_commande,id_utilisateur)
 
 	,CONSTRAINT a_pour_commande_commande_FK FOREIGN KEY (id_commande) REFERENCES public.commande(id_commande)
@@ -137,10 +137,10 @@ CREATE TABLE public.a_pour_commande(
 -- Table: ligne_panier
 ------------------------------------------------------------
 CREATE TABLE public.ligne_panier(
-	id_meuble        INT  NOT NULL ,
-	id_utilisateur   INT  NOT NULL ,
-	quantite         INT  NOT NULL ,
-	date_ajout       DATE  NOT NULL  ,
+	id_meuble        INT   ,
+	id_utilisateur   INT   ,
+	quantite         INT   ,
+	date_ajout       DATE    ,
 	CONSTRAINT ligne_panier_PK PRIMARY KEY (id_meuble,id_utilisateur)
 
 	,CONSTRAINT ligne_panier_meuble_FK FOREIGN KEY (id_meuble) REFERENCES public.meuble(id_meuble)
